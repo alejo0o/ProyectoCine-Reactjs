41723504c165b4ed9e54abf0b0fdeec68b21365c
Alejandro Vivanco <manuelinviv@gmail.com>
2020-04-10 17:11:22 -0500

    Cambios a la api

--- src/api.js
+++ src/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = 'http://localhost:8000/api';
+const BASE_URL = 'http://proyectocinegithub.herokuapp.com/api';
 
 /* const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 const randomNumber = (min = 0, max = 1) =>
