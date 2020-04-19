cf50ce58b8a3c32fa8d20b1944b2ea94396d017b
Alejandro Vivanco <manuelinviv@gmail.com>
2020-04-19 01:08:27 -0500

    Graphql se añadio forms crud

--- src/App.js
+++ src/App.js
@@ -5,20 +5,29 @@ import FormPeliculas from './Forms/FormPeliculas';
 import FormPersonas from './Forms/FormPersonas';
 import FormNoticias from './Forms/FormNoticias';
 import FormCriticas from './Forms/FormCriticas';
-
-//
+import EditPeli from './Forms/FormEditPeliculas';
+//React Router
 import { Switch, BrowserRouter, Route } from 'react-router-dom';
+//ApolloProvider para la conexion con Apollo
+import { ApolloProvider } from '@apollo/react-hooks';
+import ApolloClient from 'apollo-boost';
 
 function App() {
+  const client = new ApolloClient({
+    uri: 'http://localhost:8000/api',
+  });
   return (
-    <BrowserRouter>
-      <Switch>
-        <Route exact path='/forms/Peliculas' component={FormPeliculas} />
-        <Route exact path='/forms/Personas' component={FormPersonas} />
-        <Route exact path='/forms/Noticias' component={FormNoticias} />
-        <Route exact path='/forms/Criticas' component={FormCriticas} />
-      </Switch>
-    </BrowserRouter>
+    <ApolloProvider client={client}>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path='/forms/Peliculas' component={FormPeliculas} />
+          <Route exact path='/forms/Personas' component={FormPersonas} />
+          <Route exact path='/forms/Noticias' component={FormNoticias} />
+          <Route exact path='/forms/Criticas' component={FormCriticas} />
+          <Route exact path='/forms/:peliId/Edit' component={EditPeli}></Route>
+        </Switch>
+      </BrowserRouter>
+    </ApolloProvider>
   );
 }
 
---
+++ src/utils/consultasPeliculas.js
@@ -0,0 +1,59 @@
+//Consultas y mutaciones para la api de graphql
+import { GraphQLClient } from 'graphql-request';
+const API_URL = `https://deploy-zeit.now.sh/api`;
+const API_HEADERS = {
+  headers: {},
+};
+
+const Peticiones = {
+  ClienteGql: new GraphQLClient(API_URL, API_HEADERS),
+  createPelicula: `
+    mutation AddMovie($input: PeliculaInput!) {
+      createPelicula(input: $input) {
+        peliculasid
+        claid
+        nombre
+        fechadelanzamiento
+        duracion
+        sinopsis
+        trailer
+      }
+    }
+  `,
+  getPeliculas: `
+    {
+      getPeliculas {
+        peliculasid
+        nombre
+        sinopsis
+      }
+    }
+  `,
+  getPelicula: `
+    query getMovie($id: ID!) {
+      getPelicula(id: $id) {
+        claid
+        nombre
+        fechadelanzamiento
+        duracion
+        sinopsis
+        trailer
+      }
+    }
+  `,
+  editPelicula: `mutation EditMovie($id:ID!,$input:PeliculaEditInput!){
+  editPelicula(id:$id,input:$input){
+    claid
+    nombre
+    fechadelanzamiento
+    sinopsis
+    trailer
+    duracion
+  }
+}`,
+  deletePelicula: `mutation DeleteMovie($id:ID!){
+  deletePelicula(id:$id)
+}`,
+};
+
+export default Peticiones;
