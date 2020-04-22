06763c9db24e7773bc1641882376127d7bf4a162
Thalía Zapata <thaly18.98@gmail.com>
2020-04-22 17:25:16 -0500

    Form Personas y edit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
-import React from 'react';
+//React Router
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
+import ApolloClient from 'apollo-boost';
+//ApolloProvider para la conexion con Apollo
+import { ApolloProvider } from '@apollo/react-hooks';
+import EditPeli from './Forms/FormEditPeliculas';
+import EditarPersona from './Forms/FormEditPersonas';
+import FormCriticas from './Forms/FormCriticas';
+import FormNoticias from './Forms/FormNoticias';
 //Forms
 import FormPeliculas from './Forms/FormPeliculas';
 import FormPersonas from './Forms/FormPersonas';
-import FormNoticias from './Forms/FormNoticias';
-import FormCriticas from './Forms/FormCriticas';
-import EditPeli from './Forms/FormEditPeliculas';
-//React Router
-import { Switch, BrowserRouter, Route } from 'react-router-dom';
-//ApolloProvider para la conexion con Apollo
-import { ApolloProvider } from '@apollo/react-hooks';
-import ApolloClient from 'apollo-boost';
+import React from 'react';
 
 function App() {
   const client = new ApolloClient({
@@ -25,6 +26,7 @@ function App() {
           <Route exact path='/forms/Noticias' component={FormNoticias} />
           <Route exact path='/forms/Criticas' component={FormCriticas} />
           <Route exact path='/forms/:peliId/Edit' component={EditPeli}></Route>
+          <Route exact path='/forms/:perid/EditarPersona' component={EditarPersona}></Route>
         </Switch>
       </BrowserRouter>
     </ApolloProvider>
--- src/Forms/FormPersonas.js
+++ src/Forms/FormPersonas.js
@@ -1,5 +1,20 @@
+import 'bootstrap/dist/css/bootstrap.css';
+import './styles/navbar.css';
+
 import React, { Component } from 'react';
-import Api from '../utils/api';
+
+import { Link } from 'react-router-dom';
+import Peticiones from '../utils/consultasPersonas';
+
+function NavBar() {
+  return (
+    <div className='Navbar'>
+      <div className='container-fluid Navbar__brand'>
+        <h2 className='center'>PERSONAS</h2>
+      </div>
+    </div>
+  );
+}
 class FormPersonas extends Component {
   constructor(props) {
     super(props);
@@ -14,9 +29,13 @@ class FormPersonas extends Component {
         perfechanacim: '',
         perlugarnacim: '',
       },
+      personasList:[],
     };
   }
 
+  componentDidMount() {
+    this.fetchData();
+  }
   onChange = (e) => {
     this.setState({
       form: {
@@ -25,18 +44,16 @@ class FormPersonas extends Component {
       },
     });
   };
-
-  onSubmit = async (evt) => {
-    evt.preventDefault();
+  fetchData = async () => {
     this.setState({
       loading: true,
       error: null,
     });
-
     try {
-      await Api.personas.create(this.state.form);
+      const data = await Peticiones.ClienteGql.request(Peticiones.getPersonas);
       this.setState({
         loading: false,
+        personasList: this.state.personasList.concat(data.getPersonas),
       });
     } catch (error) {
       this.setState({
@@ -45,63 +62,104 @@ class FormPersonas extends Component {
       });
     }
   };
-
+  handleSubmit = async (evt) => {
+    evt.preventDefault();
+    this.setState({
+      loading: true,
+      error: null,
+    });
+    try {
+      const variables = {
+        input: this.state.form,
+      };
+      await Peticiones.ClienteGql.request(Peticiones.createPersona, variables);
+    } catch (error) {
+      this.setState({
+        loading: false,
+        error: error,
+      });
+      console.log(error);
+    }
+  };
+ 
   render() {
     return (
-      <div>
-        <form onSubmit={this.onSubmit}>
-          <div className='form-group'>
-            <label className='label'>Pais Id:</label>
-            <input
-              type='text'
-              name='paiid'
-              className='form-control'
-              onChange={this.onChange}
-              value={this.state.form.paiid}></input>
-          </div>
+      <React.Fragment>
+        <NavBar></NavBar>
+        <div className='container'>
+          <div className='row'>
+            <div className='col-6'>
+              <form onSubmit={this.handleSubmit}>
+                <div className='form-group'>
+                  <label className='label'>Paiid:</label>
+                  <input
+                    type='text'
+                    name='paiid'
+                    className='form-control'
+                    onChange={this.onChange}
+                    value={this.state.form.paiid}></input>
+                </div>
 
-          <div className='form-group'>
-            <label className='label'>Nombre:</label>
-            <input
-              type='text'
-              name='pernombre'
-              className='form-control'
-              onChange={this.onChange}
-              value={this.state.form.pernombre}></input>
-          </div>
+                <div className='form-group'>
+                  <label className='label'>Nombre:</label>
+                  <input
+                    type='text'
+                    name='pernombre'
+                    className='form-control'
+                    onChange={this.onChange}
+                    value={this.state.form.pernombre}></input>
+                </div>
 
-          <div className='form-group'>
-            <label className='label'>Apellido</label>
-            <input
-              type='text'
-              name='perapellido'
-              className='form-control'
-              onChange={this.onChange}
-              value={this.state.form.perapellido}></input>
-          </div>
+                <div className='form-group'>
+                  <label className='label'>Apellido:</label>
+                  <input
+                    type='text'
+                    name='perapellido'
+                    className='form-control'
+                    onChange={this.onChange}
+                    value={this.state.form.perapellido}></input>
+                </div>
 
-          <div className='form-group'>
-            <label className='label'>Fecha nacimiento:</label>
-            <input
-              type='text'
-              name='perfechanacim'
-              className='form-control'
-              onChange={this.onChange}
-              value={this.state.form.perfechanacim}></input>
-          </div>
+                <div className='form-group'>
+                  <label className='label'>Fecha nacimiento:</label>
+                  <input
+                    type='text'
+                    name='perfechanacim'
+                    className='form-control'
+                    onChange={this.onChange}
+                    value={this.state.form.perfechanacim}></input>
+                </div>
 
-          <div className='form-group'>
-            <label className='label'>Lugar nacimiento:</label>
-            <input
-              type='text'
-              name='perlugarnacim'
-              className='form-control'
-              onChange={this.onChange}
-              value={this.state.form.perlugarnacim}></input>
+                <div className='form-group'>
+                  <label className='label'>Lugar nacimiento:</label>
+                  <input
+                    type='text'
+                    name='perlugarnacim'
+                    className='form-control'
+                    onChange={this.onChange}
+                    value={this.state.form.perlugarnacim}></input>
+                </div>
+
+                <button className='btn btn-primary'  onClick={() => window.location.reload(false)} >Save</button>
+              </form>
+            </div>
+            <div className='col-6'>
+              <ul>
+                {this.state.personasList.map((persona) => {
+                  return (
+                    <li key={persona.perid}>
+                      <Link to={`${persona.perid}/EditarPersona`}>
+                        {persona.perid}
+                      </Link>
+                      <p>{persona.pernombre} {persona.perapellido}</p>
+                    </li>
+                  );
+                })}
+              </ul>
+            </div>
           </div>
-          <button className='btn btn-primary'>Save</button>
-        </form>
-      </div>
+        </div>
+      </React.Fragment>
     );
   }
 }
